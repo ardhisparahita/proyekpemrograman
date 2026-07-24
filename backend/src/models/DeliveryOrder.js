9a47706d814953c089c942b9460045d4c5f2b8e7
Ardhis Parahita <ardhisparahitaa@gmail.com>
2026-07-24 22:39:08 +0700

    feat(models): add delivery order model

---
+++ backend/src/models/DeliveryOrder.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const DeliveryOrder = sequelize.define(
+  "delivery_orders",
+  {
+    do_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+
+    admin_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+
+    driver_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+
+    destination: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+
+    delivery_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+
+    status: {
+      type: DataTypes.ENUM(
+        "PENDING",
+        "PICKING",
+        "ON_DELIVERY",
+        "COMPLETED",
+        "CANCELLED"
+      ),
+      defaultValue: "PENDING",
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = DeliveryOrder;
