216250c87af03752b09f36f67d975fdcd51b97c4
Ardhis Parahita <ardhisparahitaa@gmail.com>
2026-07-28 14:45:01 +0700

    feat(delivery-order): add delivery order repository

---
+++ backend/src/repositories/DeliveryOrderRepository.js
@@ -0,0 +1,107 @@
+const {
+    DeliveryOrder,
+    DeliveryOrderItem,
+    User,
+    Product,
+} = require("../models");
+
+class DeliveryOrderRepository {
+
+    async findAll() {
+        return await DeliveryOrder.findAll({
+            include: [
+                {
+                    model: User,
+                    as: "admin",
+                    attributes: ["id", "name", "email"],
+                },
+                {
+                    model: User,
+                    as: "driver",
+                    attributes: ["id", "name", "email"],
+                },
+                {
+                    model: DeliveryOrderItem,
+                    as: "items",
+                    include: [
+                        {
+                            model: Product,
+                            as: "product",
+                            attributes: [
+                                "id",
+                                "product_code",
+                                "product_name",
+                                "barcode",
+                                "unit",
+                            ],
+                        },
+                    ],
+                },
+            ],
+            order: [["createdAt", "DESC"]],
+        });
+    }
+
+    async findById(id) {
+        return await DeliveryOrder.findByPk(id, {
+            include: [
+                {
+                    model: User,
+                    as: "admin",
+                    attributes: ["id", "name", "email"],
+                },
+                {
+                    model: User,
+                    as: "driver",
+                    attributes: ["id", "name", "email"],
+                },
+                {
+                    model: DeliveryOrderItem,
+                    as: "items",
+                    include: [
+                        {
+                            model: Product,
+                            as: "product",
+                            attributes: [
+                                "id",
+                                "product_code",
+                                "product_name",
+                                "barcode",
+                                "unit",
+                            ],
+                        },
+                    ],
+                },
+            ],
+        });
+    }
+
+    async create(data, transaction) {
+        return await DeliveryOrder.create(data, {
+            transaction,
+        });
+    }
+
+    async createItem(data, transaction) {
+        return await DeliveryOrderItem.create(data, {
+            transaction,
+        });
+    }
+
+    async update(id, data) {
+        const deliveryOrder = await DeliveryOrder.findByPk(id);
+
+        await deliveryOrder.update(data);
+
+        return deliveryOrder;
+    }
+
+    async delete(id) {
+        const deliveryOrder = await DeliveryOrder.findByPk(id);
+
+        return await deliveryOrder.destroy();
+    }
+
+}
+
+module.exports = new DeliveryOrderRepository();
