280837a607de7eefdf72eb48b1ac46a0c7add911
Ardhis Parahita <ardhisparahitaa@gmail.com>
2026-07-28 14:08:46 +0700

    fix duplicate

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,12 @@ const inventoryRoutes = require('./src/routes/InventoryRoutes');
 const deliveryRoutes = require('./src/routes/DeliveryRoutes');
 const warehouseRoutes = require('./src/routes/WareHouseRoutes');
 const productRoutes = require('./src/routes/ProductRoutes');
-const inventoryRoutes = require('./src/routes/InventoryRoutes');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/inventory', inventoryRoutes);
 app.use('/api/delivery', deliveryRoutes);
 app.use("/api/warehouses", warehouseRoutes);
 app.use('/api/product', productRoutes);
-app.use('/api/inventory', inventoryRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
