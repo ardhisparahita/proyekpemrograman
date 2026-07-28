36d067943eb390d23aa6bcee5054b0f4502ff140
Ardhis Parahita <ardhisparahitaa@gmail.com>
2026-07-28 14:06:58 +0700

    feat(inventory): add inventory routes

--- backend/src/routes/InventoryRoutes.js
+++ backend/src/routes/InventoryRoutes.js
@@ -1,10 +1,46 @@
-const router = require('express').Router();
+const express = require("express");
+const router = express.Router();
+
+const InventoryController = require('../controllers/InventoryController');
+const validateInventory = require('../validations/inventoryValidation');
 
 const auth = require('../middleware/auth');
-const inventoryController = require('../controllers/InventoryController');
+const authorize = require('../middleware/authorize');
 
-router.get('/', auth, inventoryController.getInventory);
+router.get(
+    "/",
+    auth,
+    InventoryController.findAll
+);
 
-router.post('/', auth, inventoryController.createInventory);
+router.get(
+    "/:id",
+    auth,
+    InventoryController.findById
+);
+
+
+router.post(
+    "/",
+    auth,
+    authorize("ADMIN"),
+    validateInventory,
+    InventoryController.create
+);
+
+router.put(
+    "/:id",
+    auth,
+    authorize("ADMIN"),
+    validateInventory,
+    InventoryController.update
+);
+
+router.delete(
+    "/:id",
+    auth,
+    authorize("ADMIN"),
+    InventoryController.delete
+);
 
 module.exports = router;
