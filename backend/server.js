b3b8be22039ffc90cff799ea07191e21391d5c00
Ardhis Parahita <ardhisparahitaa@gmail.com>
2026-07-28 13:24:49 +0700

    feat(product): add product routes

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,13 @@ const authRoutes = require('./src/routes/AuthRoutes');
 const inventoryRoutes = require('./src/routes/InventoryRoutes');
 const deliveryRoutes = require('./src/routes/DeliveryRoutes');
 const warehouseRoutes = require('./src/routes/WareHouseRoutes');
+const productRoutes = require('./src/routes/ProductRoutes');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/inventory', inventoryRoutes);
 app.use('/api/delivery', deliveryRoutes);
 app.use("/api/warehouses", warehouseRoutes);
+app.use('/api/product', productRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
