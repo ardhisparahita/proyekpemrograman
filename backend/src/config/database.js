baf7deb871f6a2da862c269af0ba2e9eea6392d6
Ardhis Parahita <ardhisparahitaa@gmail.com>
2026-07-31 15:20:38 +0700

    fix

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,7 +6,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 4000,
     dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        minVersion: "TLSv1.2",
+        rejectUnauthorized: true,
+      },
+    },
   }
 );
 
