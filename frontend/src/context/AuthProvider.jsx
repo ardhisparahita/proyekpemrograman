d1825925a7f193de2146260eaefe82f787adc3ad
Ardhis Parahita <ardhisparahitaa@gmail.com>
2026-07-31 16:14:55 +0700

    fix fe

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from "react";
-import AuthContext from "./authContext";
+import AuthContext from "./AuthContext";
 import authService from "../services/authService";
 import { getToken } from "../utils/token";
 
